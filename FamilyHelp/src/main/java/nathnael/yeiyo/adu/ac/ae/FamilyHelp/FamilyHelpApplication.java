8dfe83515fd7a1197dca335e833c6849ce7a9140
YeiyoNathnael <138720671+YeiyoNathnael@users.noreply.github.com>
2026-05-06 03:28:59 +0400

    Init: Initialize a starter springboot v4.0.6 project

---
+++ FamilyHelp/src/main/java/nathnael/yeiyo/adu/ac/ae/FamilyHelp/FamilyHelpApplication.java
@@ -0,0 +1,13 @@
+package nathnael.yeiyo.adu.ac.ae.FamilyHelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyHelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyHelpApplication.class, args);
+	}
+
+}
---
+++ FamilyHelp/src/test/java/nathnael/yeiyo/adu/ac/ae/FamilyHelp/FamilyHelpApplicationTests.java
@@ -0,0 +1,13 @@
+package nathnael.yeiyo.adu.ac.ae.FamilyHelp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyHelpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
